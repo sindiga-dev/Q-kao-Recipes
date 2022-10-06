a7aa371603ba75118a00918d8d3165210f76aed1
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 21:17:25 +0300

    Imports React components to App.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import { MealsListContext } from './providers/mealsListProvider';
+
+import Meals from './components/Meals';
+import Favorites from './components/Favorites';
+import Header from './components/Header';
+import styles from './styles/App.module.css';
+import RecipeInfo from './components/RecipeInfo';
