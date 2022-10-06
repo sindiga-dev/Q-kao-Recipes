eea7760b812f5af2eea50d3320c2c81bedf22fea
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 21:36:34 +0300

    Imports react compnents to header

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import { useState, useContext } from 'react';
+import { MealsListContext } from '../providers/mealsListProvider';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/Header.module.css';
