d18e2b5cb4fce5dcebfd2b0bc32d675c0f425d00
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 21:37:28 +0300

    exports default header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,3 +3,21 @@ import { MealsListContext } from '../providers/mealsListProvider';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Header.module.css';
+
+export default function Header() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const { searchMeals } = useContext(MealsListContext);
+  
+    function handleChange(e) {
+      setSearchTerm(e.target.value);
+    }
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+  
+      if (searchTerm === '') return;
+  
+      searchMeals(searchTerm);
+  
+      setSearchTerm('');
+    }
