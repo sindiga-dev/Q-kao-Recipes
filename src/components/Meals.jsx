0c98a38816a47db185d0c7c700712108f44925cf
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 21:48:24 +0300

    Adds meals

---
+++ src/components/Meals.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { MealsListContext } from "../providers/mealsListProvider";
+import Meal from "./Meal";
+
+export default function Meals() {
+    const { meals } = useContext(MealsListContext);
+    
+    return (
+        <>
+        {meals &&
+            meals.map((meal) => <Meal key={meal.idMeal} meal={meal} />)}
+        </>
+    );
+    }
