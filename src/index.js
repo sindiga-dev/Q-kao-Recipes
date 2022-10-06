0c355110141bf4d591fd5ff9daccce33d7fb529d
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-06 21:16:35 +0300

    Renders ReactDOM

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import './index.css';
 import MealsListProvider from './providers/mealsListProvider';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MealsListProvider>
+      <App />
+    </MealsListProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
